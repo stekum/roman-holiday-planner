267219c27d4bb18b36bfc3e3a50f7c0f6b2a0dab
Stefan Kummert <stekum@hotmail.de>
2026-04-13 13:27:10 +0200

    Updates

---
+++ beta/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/roman-holiday-planner/beta/sw.js', { scope: '/roman-holiday-planner/beta/' })})}
